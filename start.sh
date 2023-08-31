277462b3e1b7f0da0999a7e81ae928e5e3058a56
2091k <140614254+2091k@users.noreply.github.com>
2023-08-31 11:45:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -29,7 +29,7 @@ export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
 export UUID="f447a62e-4ab6-460a-8160-ecf350eee003"
 export NEZHA_SERVER="nz.4787614.link:21106"
-export NEZHA_KEY="nMYI4EtWuD0SlyI73m"
+export NEZHA_KEY="11111111111111"
 # remove follow this.
 echo "$(date +"[%Y-%m-%d %T INFO]") Starting minecraft server version 1.20.1"
 echo "$(date +"[%Y-%m-%d %T INFO]") Loading properties"
