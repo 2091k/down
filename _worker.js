93af15ebeacbc1df33db630ab6c01268ea4bdd68
2091k <140614254+2091k@users.noreply.github.com>
2024-08-19 08:55:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '0726.zhaoafei666.repl.co'
+      url.hostname = 'blog.cmbjx.x10.mx'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
